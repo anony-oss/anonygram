5cb02eb3f5cee6a425545d636d195353b0dde006
Vlad Pavlyuchkov <64595338+Vlad10081@users.noreply.github.com>
2022-03-15 10:46:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = '/Users/vlad/Desktop/Main/Projects/AnonyGram/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, template_folder='templates')
