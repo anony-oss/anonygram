e2cc29259260ad858ba6ea206d47986baba57249
Vlad Pavlyuchkov <pavlyuchkov11@bk.ru>
2022-03-24 23:45:49 +0300

    bug fix

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from .extensions import cors, db, migrate, start_extensions
 def create_app():
     app: Flask = Flask(__name__)
     
-    app.config.from_pyfile('/Users/vlad/Desktop/Main/Projects/AnonyGram/project/config.py')
+    app.config.from_pyfile('config.py')
     
     start_extensions(app)
     return app
--- server/config.py
+++ server/config.py
@@ -1,5 +1,5 @@
 SECRET_KEY = 'fJjfKDSJfsKIOEOIRUEKODCMKsvmgfdjfkDSJHBgijJKHSDJGfjJHSFIGjkdjhFHIdhjshjkIFEHUiuwiqeoipreopwkodmvMK'
-UPLOAD_FOLDER = '/Users/vlad/Desktop/Main/Projects/AnonyGram/uploads'
+UPLOAD_FOLDER = 'uploads'
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:4253@localhost/anonygram'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
