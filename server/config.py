01f27711fda3aba0c6776c17df453ce41f2ac8ce
Vlad Pavlyuchkov <pavlyuchkov11@bk.ru>
2022-03-25 13:05:46 +0300

    fix db

--- server/config.py
+++ server/config.py
@@ -1,5 +1,6 @@
 SECRET_KEY = 'fJjfKDSJfsKIOEOIRUEKODCMKsvmgfdjfkDSJHBgijJKHSDJGfjJHSFIGjkdjhFHIdhjshjkIFEHUiuwiqeoipreopwkodmvMK'
 UPLOAD_FOLDER = 'uploads'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:4253@localhost/anonygram'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///main.db'
+#SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:4253@localhost/anonygram'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
